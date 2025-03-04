bcfdb657b9f3c208ea6f16132b1a248bba29e768
JoseDv1 <Jose123millon@gmail.com>
2025-03-03 21:57:50 -0500

    Refactor hono-astro-adapter integration to streamline configuration and improve image service handling

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,42 @@
 import type { AstroAdapter, AstroIntegration } from "astro";
+import { passthroughImageService } from "astro/config";
 
-const honoAstro: AstroIntegration = {
-	name: "hono-astro-adapter",
-	hooks: {
-		"astro:config:setup": async ({ updateConfig }) => {
-			updateConfig({
-				vite: {
-					ssr: {
-						noExternal: ["hono-astro-adapter"],
+export default function createIntegration(): AstroIntegration {
+	return {
+		name: "hono-astro-adapter",
+		hooks: {
+			"astro:config:setup": async ({ updateConfig }) => {
+				updateConfig({
+					image: {
+						service: passthroughImageService(),
 					},
-					build: {
-						rollupOptions: {
-							external: ["hono-astro-adapter"],
+					adapter: {
+						name: "hono-astro-adapter",
+					},
+					vite: {
+						ssr: {
+							noExternal: ["hono-astro-adapter"],
 						},
+						build: {
+							rollupOptions: {
+								external: ["hono-astro-adapter"],
+							},
+						},
+					}
+				});
+			},
+			"astro:config:done": async ({ setAdapter }) => {
+				setAdapter({
+					name: "hono-astro-adapter",
+					supportedAstroFeatures: {
+						serverOutput: "stable",
+						staticOutput: "stable",
+						envGetSecret: "stable",
 					},
-				}
-			});
+					serverEntrypoint: "hono-astro-adapter/server.js",
+					exports: ["handler"],
+				});
+			}
 		},
-		"astro:config:done": async ({ setAdapter }) => {
-			setAdapter(astroAdapter);
-		}
-	},
-}
-
-const astroAdapter: AstroAdapter = {
-	name: "hono-astro-adapter",
-	supportedAstroFeatures: {
-		serverOutput: "stable",
-		staticOutput: "stable",
-		hybridOutput: "stable",
-		assets: {
-			supportKind: "stable",
-			isSharpCompatible: true,
-			isSquooshCompatible: true,
-		},
-		i18nDomains: "experimental",
-		envGetSecret: "experimental",
-	},
-	exports: ["handler"],
-	serverEntrypoint: "hono-astro-adapter/server.js",
-}
-
-export default function createIntegration() {
-	return honoAstro;
+	}
 }
