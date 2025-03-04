4e4b8cd266410a68d42e2e7cf512d952f95dce9a
JoseDv1 <Jose123millon@gmail.com>
2025-03-03 21:58:06 -0500

    Agregar configuración de entorno para el middleware en el servidor

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import { App } from "astro/app";
 import { createMiddleware } from "./createMiddleware";
 import type { SSRManifest } from "astro";
+import { setGetEnv } from "astro/env/setup";
 
+setGetEnv((key: string) => process.env[key]);
 
 export function createExports(manifest: SSRManifest) {
 	const app = new App(manifest);
